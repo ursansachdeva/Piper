8713f61aba27413032f4cb3064c23c4024373f3f
Ursan <43223748+ursansachdeva@users.noreply.github.com>
2020-02-17 14:23:12 +1100

    Update config-example.py

--- Day 1/Lab 06 - ECS/config-example.py
+++ Day 1/Lab 06 - ECS/config-example.py
@@ -1,6 +1,6 @@
 ecs_test_drive = {
     'ecs_endpoint_url' : 'https://object.ecstestdrive.com',
-    'ecs_access_key_id' : '1234-your-unique-number-5678@ecstestdrive.emc.com',
-    'ecs_secret_key' : 'your-long-secret-key-from-ECS-testdrive-portal',
+    'ecs_access_key_id' : '131130088195835410@ecstestdrive.emc.com',
+    'ecs_secret_key' : 'UK5xErUfAG2I1aL4N+I9i6JSIFeuzSt2wHrsZlB9',
     'ecs_bucket_name' : 'photo-album'
 }
