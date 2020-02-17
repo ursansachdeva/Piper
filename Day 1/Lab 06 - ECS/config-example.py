c2d994cb46088c171f83ccb096e488e047df3e60
Ursan <43223748+ursansachdeva@users.noreply.github.com>
2020-02-17 14:33:23 +1100

    Delete config-example.py

--- Day 1/Lab 06 - ECS/config-example.py
+++
@@ -1,6 +0,0 @@
-ecs_test_drive = {
-    'ecs_endpoint_url' : 'https://object.ecstestdrive.com',
-    'ecs_access_key_id' : '131130088195835410@ecstestdrive.emc.com',
-    'ecs_secret_key' : 'UK5xErUfAG2I1aL4N+I9i6JSIFeuzSt2wHrsZlB9',
-    'ecs_bucket_name' : 'photo-album'
-}
